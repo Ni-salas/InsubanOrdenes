4a575fdcff430e951bdb41cf409e75eef8e3e23b
Ni-salas <nsalas@insuban.cl>
2024-12-26 13:35:03 -0300

    Delete Prueba1 directory

--- Prueba1/ProyectoJS/backend/get_paises.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('db.php');
-$query = "SELECT cod_pais, nombre FROM Pais";
-$result = $pdo->query($query);
-
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<option value='{$row['cod_pais']}'>{$row['nombre']}</option>";
-}
-?>
--- Prueba1/ProyectoJS/backend/get_tipo_venta.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('db.php');
-$query = "SELECT cod_venta, descripcion FROM TpVenta";
-$result = $pdo->query($query);
-
-while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-    echo "<option value='{$row['cod_venta']}'>{$row['descripcion']}</option>";
-}
-?>
